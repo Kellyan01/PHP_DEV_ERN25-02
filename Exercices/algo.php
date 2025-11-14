0af4a5c2dca9a5a5807ba56b76d4fd961d974ac7
Kellyan01 <depriester.yoann@neuf.fr>
2025-11-14 11:55:35 +0100

    [Exercice] Correction Exercice Algo 10 -> 14

--- Exercices/algo.php
+++ Exercices/algo.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <header>
+    <header id='header'>
         <nav>
             <ul>
                 <li><a href="../index.php">Accueil</a></li>
@@ -19,6 +19,10 @@
                 <li><a href="#exercice07">Exercice 07</a></li>
                 <li><a href="#exercice08">Exercice 08</a></li>
                 <li><a href="#exercice09">Exercice 09</a></li>
+                <li><a href="#exercice10">Exercice 10</a></li>
+                <li><a href="#exercice11">Exercice 11</a></li>
+                <li><a href="#exercice12">Exercice 12</a></li>
+                <li><a href="#exercice13">Exercice 13</a></li>
             </ul>
         </nav>
     </header>
@@ -174,6 +178,166 @@
             }
             echo "<p>La somme de 1, 2 et 3 donne : ".addition(1,2,3)."</p>";
         ?>
+
+        <h2 id="exercice10">EXERCICE 10</h2>
+        <?php
+            /*Exercice 10 :
+            -Créer une fonction qui teste si un nombre est positif ou négatif (echo dans la page web).*/
+            function positif($nbr){
+                if($nbr < 0){
+                    echo "<p>$nbr est négatif.</p>";
+                }else if($nbr > 0){
+                    echo "<p>$nbr est positif.</p>";
+                }else{
+                    echo "<p>$nbr est nulle.</p>";
+                }
+            }
+            positif(5);
+            positif(-5);
+            positif(0);
+        ?>
+
+        <h2 id="exercice11">EXERCICE 11</h2>
+        <?php
+            /*Exercice 11 :
+            -Créer une fonction qui prend en entrée 3 valeurs et retourne le nombre le plus grand (echo dans la page web).*/
+            function plusGrand($a, $b, $c){
+                if($a>=$b and $a>=$c){
+                    echo "<p>$a est le plus grand.</p>";
+                }else if($b>=$a and $b>=$c){
+                    echo "<p>$b est le plus grand.</p>";
+                }else{
+                    echo "<p>$c est le plus grand.</p>";
+                }
+            }
+
+            plusGrand(1,2,3);
+            plusGrand(5,4,3);
+            plusGrand(7,9,8);
+        ?>
+
+        <h2 id="exercice12">EXERCICE 12</h2>
+        <?php
+            /*Exercice 12 :
+            Créer une fonction qui prend en entrée 3 valeurs et retourne le nombre le plus petit (echo dans la page web).*/
+            function plusPetit($a, $b, $c){
+                if($a<=$b and $a<=$c){
+                    echo "<p>$a est le plus petit.</p>";
+                }else if($b<=$a and $b<=$c){
+                    echo "<p>$b est le plus petit.</p>";
+                }else{
+                    echo "<p>$c est le plus petit.</p>";
+                }
+            }
+
+            plusPetit(1,2,3);
+            plusPetit(5,4,3);
+            plusPetit(7,9,8);
+        ?>
+
+        <h2 id="exercice13">EXERCICE 13</h2>
+        <?php
+            /*Exercice 13 :
+                -Créer une fonction qui prend en entrée 1 valeur (l’âge d’un enfant). Ensuite, elle informe de sa catégorie (echo dans
+                la page web) :
+                x "Poussin" de 6 à 7 ans
+                x "Pupille" de 8 à 9 ans
+                x "Minime" de 10 à 11 ans
+                x "Cadet" après 12 ans*/
+                function categorie($age){
+                    if($age >= 6 and $age<=7){
+                        echo "<p>Poussin</p>";
+                    }else if($age >= 8 and $age<=9){
+                        echo "<p>Pupille</p>";
+                    }else if($age >= 10 and $age<=11){
+                        echo "<p>Minime</p>";
+                    }else if($age >= 12){
+                        echo "<p>Cadet</p>";
+                    }else{
+                        echo "<p>Hors Catégorie</p>";
+                    }
+                }
+                categorie(5);
+                categorie(6);
+                categorie(7);
+                categorie(8);
+                categorie(9);
+                categorie(10);
+                categorie(11);
+                categorie(12);
+                categorie(30);
+        ?>
+
+        <h2 id="exercice14">EXERCICE 14</h2>
+        <?php
+            /*Exercice 14 :
+            Refaire l’exercice 13 en utilisant le switch case.*/
+            function categorieSwitch($age){
+                    switch($age){
+                        case 6 :
+                        case 7 :
+                            echo "<p>Poussin</p>";
+                            break;
+                        case 8 :
+                        case 9 :
+                            echo "<p>Pupille</p>";
+                            break;
+                        case 10 :
+                        case 11 :
+                            echo "<p>Minime</p>";
+                            break;
+                        case 12 :
+                            echo "<p>Cadet</p>";
+                            break;
+                        default :
+                            echo "<p>Hors Catégorie</p>";
+                            break;
+                    }
+            }
+                categorieSwitch(5);
+                categorieSwitch(6);
+                categorieSwitch(7);
+                categorieSwitch(8);
+                categorieSwitch(9);
+                categorieSwitch(10);
+                categorieSwitch(11);
+                categorieSwitch(12);
+                categorieSwitch(30);
+
+            function categorieSwitchNinja($age){
+                    switch(true){
+                        case ($age >= 6 and $age <= 7) :
+                            echo "<p>Poussin</p>";
+                            break;
+                        case ($age >= 8 and $age <= 9) :
+                            echo "<p>Pupille</p>";
+                            break;
+                        case ($age >= 10 and $age <= 11) :
+                            echo "<p>Minime</p>";
+                            break;
+                        case ($age >= 12) :
+                            echo "<p>Cadet</p>";
+                            break;
+                        default :
+                            echo "<p>Hors Catégorie</p>";
+                            break;
+                    }
+            }
+            echo "<p>------ SWITCH NINJA ------</p>";
+            categorieSwitchNinja(5);
+            categorieSwitchNinja(6);
+            categorieSwitchNinja(7);
+            categorieSwitchNinja(8);
+            categorieSwitchNinja(9);
+            categorieSwitchNinja(10);
+            categorieSwitchNinja(11);
+            categorieSwitchNinja(12);
+            categorieSwitchNinja(30);
+        ?>
+
     </main>
+    <footer>
+        <a href="#header" style="position:fixed; bottom:1em; right:1em; background-color:#333; color:#fff; padding:5px">RETOUR EN HAUT</a>
+    </footer>
 </body>
 </html>
